18700fd802ad256c285ffde7b36c83e43424d968
Hazelcode <srirambalaaje@gmail.com>
2026-06-10 21:00:23 +0530

    Create project1.py for card game setup
    Initialize a card game with card suits and a play class.

---
+++ project1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random as rd
+
+cards_no = {"spade":13,"clubs":13, "Hearts":13, "Diamonds":13}
+cards = "ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, king, queen"
+
+class play:
+  def __inti__(self,cards):
+    self.spade = ""
+    self.clubs = ""
+    self.hearts = ""
+    self.diamonds = ""
+  def info(self,cards):
+    self.spade = spade
+    self.clubs = clubs
+    self.diamnond = diamond
+    self.hearts = hearts
+
+
+My_cards = play(cards)
+My_cards.add(cards)
+
+
+
