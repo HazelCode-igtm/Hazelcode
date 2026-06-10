7382768c00670ab6c5f89997fe7426b6e1983f0b
Hazelcode <srirambalaaje@gmail.com>
2026-06-10 21:22:50 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,24 +1,30 @@
-import numpy as np
 import random as rd
 
 cards_no = {"spade":13,"clubs":13, "Hearts":13, "Diamonds":13}
-cards = "ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, king, queen"
+cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "king", "queen"]
 
 class play:
-  def __inti__(self,cards):
-    self.spade = ""
-    self.clubs = ""
-    self.hearts = ""
-    self.diamonds = ""
-  def info(self,cards):
-    self.spade = spade
-    self.clubs = clubs
-    self.diamnond = diamond
-    self.hearts = hearts
+  def __init__(self,cards):
+    self.spade = cards.copy()
+    self.clubs = cards.copy()
+    self.hearts = cards.copy()
+    self.diamonds = cards.copy()
+
+  def no (self):
+    self.spade_no = 13
+    self.clubs_no = 13
+    self.hearts_no = 13
+    self.diamonds_no = 13
 
 
+  def show(self):
+    print(f"spade: {self.spade} has {self.spade_no} cardS")
+    
 My_cards = play(cards)
-My_cards.add(cards)
+My_cards.no()
+My_cards.show()
+
+
 
 
 
